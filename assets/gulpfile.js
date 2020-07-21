8670147e6102c33fd5fafb2f85c47b8af22d9b1c
Rafaela B. T <barbosa.trindade@ieee.org>
2020-07-21 06:06:22 -0300

    Teste

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,11 +1,11 @@
 /* global require */
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+
 sass.compiler = require('node-sass');
- 
+
 gulp.task('sass', function () {
  return gulp.src('sass/**/*.sass')
    .pipe(sass().on('error', sass.logError))
