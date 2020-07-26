104abf72a14c53e53590f9e0e9edb272899a6269
Rafaela B. T <barbosa.trindade@ieee.org>
2020-07-26 16:18:55 -0300

    diretoria - colocando animação de exemplo

--- assets/js/diretoria.js
+++ assets/js/diretoria.js
@@ -5,6 +5,7 @@ for (let i = 0; i < diretoria.length; i++){
     const ilustracao = document.getElementsByClassName('ilustracao')[i];
     const poligono = ilustracao.getElementsByClassName('cls-1')[0];
     const redes_sociais = document.getElementsByClassName('icones')[i];
+    redes_sociais.style.animation = 'opacidade 1s';
     redes_sociais.style.display = 'block';
     poligono.setAttribute('d', 'M0,0,102.5,65,205,0V46.75l-102.5,65L0,46.75Z');
   });
@@ -12,6 +13,7 @@ for (let i = 0; i < diretoria.length; i++){
     const ilustracao = document.getElementsByClassName('ilustracao')[i];
     const poligono = ilustracao.getElementsByClassName('cls-1')[0];
     const redes_sociais = document.getElementsByClassName('icones')[i];
+    redes_sociais.style.animation = 'none';
     redes_sociais.style.display = 'none';
     poligono.setAttribute('d', 'M0,0,102.5,65,205,0V7.58l-102.5,65L0,7.58Z');
   });
